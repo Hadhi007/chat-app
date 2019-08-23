38b9e36a320331f4b2071b0e0c6c30e85e661b60
Femisha Jasmi <femisha.jasmi@aethanalytica.com>
2019-08-23 15:00:19 +0530

    Messages are passing from one user to another

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const socket=io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
-    socket.on('chat-message', data =>{ 
-        console.log(data)
-    })
-messageForm.addEventListener('submit', e => {
-    e.preventDefault()=messageInput.value
-    socket.emit('send-chat-message',message)
-    messageInput.value=''
+const name=prompt('what is your name ?')
+appendMessage('You Joined')
+socket.emit('new user',name)
+
+socket.on('chat-message', data =>{ 
+    appendMessage(data)
 })
+
+
+socket.on('user-connected',name =>{ 
+    appendMessage(`${name} connected`)
+})
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    socket.emit('send-chat-message',message)
+  
+})
+function appendMessage(message){
+    const messageElement =document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
