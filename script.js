9b074e27b6926130c066ea39f6f42645d2ee4221
Femisha Jasmi <femisha.jasmi@aethanalytica.com>
2019-08-23 15:30:26 +0530

    real-time chatting app completed

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-const socket=io('http://localhost:3000')
+const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
-const name=prompt('what is your name ?')
+const name = prompt('what is your name ?')
 appendMessage('You Joined')
-socket.emit('new user',name)
+socket.emit('new user', name)
 
-socket.on('chat-message', data =>{ 
-    appendMessage(data)
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}:${data.message}`)
 })
 
 
-socket.on('user-connected',name =>{ 
+socket.on('user-connected', name => {
     appendMessage(`${name} connected`)
+})
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
 })
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
-    socket.emit('send-chat-message',message)
-  
+    appendMessage(`You:${message}`)
+    socket.emit('send-chat-message', message)
+
 })
-function appendMessage(message){
-    const messageElement =document.createElement('div')
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
     messageElement.innerText = message
     messageContainer.append(messageElement)
 }
--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const users ={}
 
 
 io.on('connection', socket => {
-    socket.emit('new user',name => {
+    socket.on('new user',name => {
     users[socket.id]=name
     socket.broadcast.emit('user-connected',name)
     })
-    socket.on('send-chat-message', message =>{
-        socket.broadcast.emit('chat-message',message)
-    })
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+      })
+    socket.on('disconnect',name => {
+        socket.broadcast.emit('user-disconnected',users[socket.id])
+         delete users[socket.id]
+    
+        })
 })
